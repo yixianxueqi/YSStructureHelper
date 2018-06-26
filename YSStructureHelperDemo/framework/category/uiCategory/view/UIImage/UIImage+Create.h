c0ed9bc5231af13dc260b7ea47e0ba6c65c9a70c
Edward <edward.tang@5kwords.com>
2018-06-26 11:04:46 +0800

    添加加载图片方式和处理，ImageIO

--- YSStructureHelperDemo/framework/category/uiCategory/view/UIImage/UIImage+Create.h
+++ YSStructureHelperDemo/framework/category/uiCategory/view/UIImage/UIImage+Create.h
@@ -19,6 +19,8 @@
  */
 + (UIImage *)getImageFromColor:(UIColor *)color;
 
+#pragma mark - 根据视图生成图片
+
 /**
  用view生成图片，大小与view大小相同
  @note view及其子视图需布局完毕
@@ -40,4 +42,57 @@
 + (UIImage *)getImageFromLayer:(CALayer *)layer;
 // 同 + (UIImage *)getImageFromView:(UIView *)view area:(CGRect)rect;
 + (UIImage *)getImageFromLayer:(CALayer *)layer area:(CGRect)rect;
+
+
+#pragma mark - 加载图片
+
+/**
+ 通过ImageIO根据URL获取图片
+ @note 通过imageIO加载图片，减少内存消耗
+ https://www.jianshu.com/p/a6c673678f36
+ 测试：iPhone7 11.2.6系统下，Xcode9.4
+ 本地文件148KB的jpg图片：
+ 通过[UIImage imageWithContentsOfFile:path]加载内存占用为15.7MB左右，
+ 通过ImageIO加载，内存占用为13.2MB左右
+ 对比sdwebImage：
+ 加载本地图片4张：
+ 通过SDWebImage，内存占用为15MB左右
+ 通过ImageIO加载，内存占用为14MB左右
+ 加载网络图片：
+ 通过SDWebImage，内存占用为14MB左右
+ 通过ImageIO加载，内存占用为14.5MB左右
+ @param url 图片url
+ @return UIImage
+ */
++ (UIImage *)getImageFromURl:(NSURL *)url ;
+
+/**
+ 通过ImageIO根据URL获取图片
+ 
+ @param url 图片url
+ @param scale 缩放比例
+ @return UIImage
+ */
++ (UIImage *)getImageFromURL:(NSURL *)url scale:(CGFloat)scale;
+
+
+/**
+ 通过ImageIO根据URL获取图片
+ 
+ @param url 图片url
+ @param maxLength 最大长度，创建缩略图等比缩放大小，会根据长宽值比较大的作为imageSize进行缩放
+ @return UIImage
+ */
++ (UIImage *)getImageFromURL:(NSURL *)url maxPixelSize:(NSUInteger)maxLength;
+
+#pragma mark - 修改图片
+
+/**
+ 修改图片尺寸
+ 
+ @param scale 比例
+ @return UIImage
+ */
+- (UIImage *)resizeScaleImage:(CGFloat)scale;
+
 @end
--- YSStructureHelperDemo/framework/category/uiCategory/view/UIImage/UIImage+Create.m
+++ YSStructureHelperDemo/framework/category/uiCategory/view/UIImage/UIImage+Create.m
@@ -22,6 +22,8 @@
     return img;
 }
 
+#pragma mark - 根据视图生成图片
+
 + (UIImage *)getImageFromView:(UIView *)view {
     
     return [self getImageFromLayer:view.layer];
@@ -45,6 +47,153 @@
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return image;
+}
+
+#pragma mark - 加载图片
+
++ (UIImage *)getImageFromURl:(NSURL *)url {
+    
+    return [self getImageFromURL:url scale:1.0];
+}
+
++ (UIImage *)getImageFromURL:(NSURL *)url scale:(CGFloat)scale {
+    
+    CFURLRef cfurl = CFBridgingRetain(url);
+    CFStringRef optionKeys[1];
+    CFTypeRef optionValues[4];
+    optionKeys[0] = kCGImageSourceShouldCache;
+    optionValues[0] = (CFTypeRef)kCFBooleanFalse;
+    CFDictionaryRef sourceOption = CFDictionaryCreate(kCFAllocatorDefault, (const void **)optionKeys, (const void **)optionValues, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    CGImageSourceRef imageSource = CGImageSourceCreateWithURL(cfurl, sourceOption);
+    CFRelease(sourceOption);
+    if (!imageSource) {
+        NSLog(@"imageSource is Null!");
+        return nil;
+    }
+    //获取原图片属性
+    CFDictionaryRef property = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil);
+    NSDictionary *propertys = CFBridgingRelease(property);
+    CGFloat height = [propertys[@"PixelHeight"] integerValue];
+    CGFloat width = [propertys[@"PixelWidth"] integerValue];
+    int imageSize = (int)MAX(height * scale, width * scale);
+    CFStringRef keys[5];
+    CFTypeRef values[5];
+    //创建缩略图等比缩放大小，会根据长宽值比较大的作为imageSize进行缩放
+    keys[0] = kCGImageSourceThumbnailMaxPixelSize;
+    CFNumberRef thumbnailSize = CFNumberCreate(NULL, kCFNumberIntType, &imageSize);
+    values[0] = (CFTypeRef)thumbnailSize;
+    keys[1] = kCGImageSourceCreateThumbnailFromImageAlways;
+    values[1] = (CFTypeRef)kCFBooleanTrue;
+    keys[2] = kCGImageSourceCreateThumbnailWithTransform;
+    values[2] = (CFTypeRef)kCFBooleanTrue;
+    keys[3] = kCGImageSourceCreateThumbnailFromImageIfAbsent;
+    values[3] = (CFTypeRef)kCFBooleanTrue;
+    keys[4] = kCGImageSourceShouldCacheImmediately;
+    values[4] = (CFTypeRef)kCFBooleanTrue;
+    
+    CFDictionaryRef options = CFDictionaryCreate(kCFAllocatorDefault, (const void **)keys, (const void **)values, 4, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    CGImageRef thumbnailImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options);
+    UIImage *resultImg = [UIImage imageWithCGImage:thumbnailImage];
+    
+    CFRelease(cfurl);
+    CFRelease(thumbnailSize);
+    CFRelease(options);
+    CFRelease(imageSource);
+    CFRelease(thumbnailImage);
+    
+    return resultImg;
+}
+
++ (UIImage *)getImageFromURL:(NSURL *)url maxPixelSize:(NSUInteger)maxLength {
+    
+    CFURLRef cfurl = CFBridgingRetain(url);
+    CFStringRef optionKeys[1];
+    CFTypeRef optionValues[4];
+    optionKeys[0] = kCGImageSourceShouldCache;
+    optionValues[0] = (CFTypeRef)kCFBooleanFalse;
+    CFDictionaryRef sourceOption = CFDictionaryCreate(kCFAllocatorDefault, (const void **)optionKeys, (const void **)optionValues, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    CGImageSourceRef imageSource = CGImageSourceCreateWithURL(cfurl, sourceOption);
+    CFRelease(sourceOption);
+    if (!imageSource) {
+        NSLog(@"imageSource is Null!");
+        return nil;
+    }
+    int imageSize = (int)maxLength;
+    CFStringRef keys[5];
+    CFTypeRef values[5];
+    //创建缩略图等比缩放大小，会根据长宽值比较大的作为imageSize进行缩放
+    keys[0] = kCGImageSourceThumbnailMaxPixelSize;
+    CFNumberRef thumbnailSize = CFNumberCreate(NULL, kCFNumberIntType, &imageSize);
+    values[0] = (CFTypeRef)thumbnailSize;
+    keys[1] = kCGImageSourceCreateThumbnailFromImageAlways;
+    values[1] = (CFTypeRef)kCFBooleanTrue;
+    keys[2] = kCGImageSourceCreateThumbnailWithTransform;
+    values[2] = (CFTypeRef)kCFBooleanTrue;
+    keys[3] = kCGImageSourceCreateThumbnailFromImageIfAbsent;
+    values[3] = (CFTypeRef)kCFBooleanTrue;
+    keys[4] = kCGImageSourceShouldCacheImmediately;
+    values[4] = (CFTypeRef)kCFBooleanTrue;
+    
+    CFDictionaryRef options = CFDictionaryCreate(kCFAllocatorDefault, (const void **)keys, (const void **)values, 4, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    CGImageRef thumbnailImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options);
+    UIImage *resultImg = [UIImage imageWithCGImage:thumbnailImage];
+    
+    CFRelease(cfurl);
+    CFRelease(thumbnailSize);
+    CFRelease(options);
+    CFRelease(imageSource);
+    CFRelease(thumbnailImage);
+    
+    return resultImg;
+}
+
+#pragma mark - 修改图片
+
+- (UIImage *)resizeScaleImage:(CGFloat)scale {
+    
+    CGSize imgSize = self.size;
+    CGSize targetSize = CGSizeMake(imgSize.width * scale, imgSize.height * scale);
+    NSData *imageData = UIImageJPEGRepresentation(self, 1.0);
+    CFDataRef data = (__bridge CFDataRef)imageData;
+    
+    CFStringRef optionKeys[1];
+    CFTypeRef optionValues[4];
+    optionKeys[0] = kCGImageSourceShouldCache;
+    optionValues[0] = (CFTypeRef)kCFBooleanFalse;
+    CFDictionaryRef sourceOption = CFDictionaryCreate(kCFAllocatorDefault, (const void **)optionKeys, (const void **)optionValues, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    CGImageSourceRef imageSource = CGImageSourceCreateWithData(data, sourceOption);
+    CFRelease(sourceOption);
+    if (!imageSource) {
+        NSLog(@"imageSource is Null!");
+        return nil;
+    }
+    //获取原图片属性
+    int imageSize = (int)MAX(targetSize.height, targetSize.width);
+    CFStringRef keys[5];
+    CFTypeRef values[5];
+    //创建缩略图等比缩放大小，会根据长宽值比较大的作为imageSize进行缩放
+    keys[0] = kCGImageSourceThumbnailMaxPixelSize;
+    CFNumberRef thumbnailSize = CFNumberCreate(NULL, kCFNumberIntType, &imageSize);
+    values[0] = (CFTypeRef)thumbnailSize;
+    keys[1] = kCGImageSourceCreateThumbnailFromImageAlways;
+    values[1] = (CFTypeRef)kCFBooleanTrue;
+    keys[2] = kCGImageSourceCreateThumbnailWithTransform;
+    values[2] = (CFTypeRef)kCFBooleanTrue;
+    keys[3] = kCGImageSourceCreateThumbnailFromImageIfAbsent;
+    values[3] = (CFTypeRef)kCFBooleanTrue;
+    keys[4] = kCGImageSourceShouldCacheImmediately;
+    values[4] = (CFTypeRef)kCFBooleanTrue;
+    
+    CFDictionaryRef options = CFDictionaryCreate(kCFAllocatorDefault, (const void **)keys, (const void **)values, 4, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    CGImageRef thumbnailImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options);
+    UIImage *resultImg = [UIImage imageWithCGImage:thumbnailImage];
+    
+    CFRelease(thumbnailSize);
+    CFRelease(options);
+    CFRelease(imageSource);
+    CFRelease(thumbnailImage);
+    
+    return resultImg;
 }
 
 @end
